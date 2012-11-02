e689cca5a468eaeb5e4225c802d1dd8a54aa4ce2
ronsavage <ron@savage.net.au>
2012-11-02 14:40:56 +1100

    Clean up demo generation

--- scripts/generate.demo.sh
+++ scripts/generate.demo.sh
@@ -8,6 +8,6 @@ perl -Ilib scripts/stt2html.pl
 
 # $DR is my web server's doc root and $PM is Perl-modules/html/graphviz2.marpa.
 
-cp html/*.html html/*.svg $DR/Perl-modules/html/graphviz2.pathutils/
+cp html/*.html html/*.svg $DR/$PM
 
 echo Check the version number in the demo index
