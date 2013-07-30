dbd21eacd728166861c4b346a33b9ff5158d2330
ronsavage <ron@savage.net.au>
2013-07-30 14:37:48 +1000

    Add scripts/pod2html.sh

--- scripts/pod2html.sh
+++ scripts/pod2html.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-pod2html.pl -i lib/GraphViz2/Marpa.pm                    -o /dev/shm/html/Perl-modules/html/GraphViz2/Marpa.html
-pod2html.pl -i lib/GraphViz2/Marpa/Lexer.pm              -o /dev/shm/html/Perl-modules/html/GraphViz2/Marpa/Lexer.html
-pod2html.pl -i lib/GraphViz2/Marpa/Lexer/DFA.pm          -o /dev/shm/html/Perl-modules/html/GraphViz2/Marpa/Lexer/DFA.html
-pod2html.pl -i lib/GraphViz2/Marpa/Parser.pm             -o /dev/shm/html/Perl-modules/html/GraphViz2/Marpa/Parser.html
-pod2html.pl -i lib/GraphViz2/Marpa/Renderer/GraphViz2.pm -o /dev/shm/html/Perl-modules/html/GraphViz2/Marpa/Renderer/GraphViz2.html
-pod2html.pl -i lib/GraphViz2/Marpa/Utils.pm              -o /dev/shm/html/Perl-modules/html/GraphViz2/Marpa/Utils.html
+DEST=$DR/Perl-modules/html/GraphViz2
+
+pod2html.pl -i lib/GraphViz2/Marpa.pm                    -o $DEST/Marpa.html
+pod2html.pl -i lib/GraphViz2/Marpa/Config.pm             -o $DEST/Marpa/Config.html
+pod2html.pl -i lib/GraphViz2/Marpa/Lexer.pm              -o $DEST/Marpa/Lexer.html
+pod2html.pl -i lib/GraphViz2/Marpa/Parser.pm             -o $DEST/Marpa/Parser.html
+pod2html.pl -i lib/GraphViz2/Marpa/Utils.pm              -o $DEST/Marpa/Utils.html
+pod2html.pl -i lib/GraphViz2/Marpa/Lexer/DFA.pm          -o $DEST/Marpa/Lexer/DFA.html
+pod2html.pl -i lib/GraphViz2/Marpa/Renderer/GraphViz2.pm -o $DEST/Marpa/Renderer/GraphViz2.html
