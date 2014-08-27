43dece8427c626614dfe8b5c96faa42a549fe71e
ronsavage <ron@savage.net.au>
2014-08-27 13:47:29 +1000

    Add scripts/test.sh

--- scripts/g2m.sh
+++ scripts/g2m.sh
@@ -3,10 +3,8 @@
 # Name: g2m.sh.
 #
 # Parameters:
-# 1: The abbreviated name of sample input and output data files.
-#	E.g. 19 simultaneously means data/19.gv, data/19.lex and data/19.parse.
-# 2 & 3: Use to run the default renderer. E.g.: -o data/19.rend.
-#	Or, use to report the forest. E.g.: -report_forest 1
-#	Or, use for debugging. E.g.: -maxlevel debug.
+# 1: The name of input file.
+#	E.g. data/19.gv.
+# 2 .. N: Use for debugging etc. E.g.: -maxlevel debug.
 
 perl -Ilib scripts/g2m.pl -input_file $1 $2 $3 $4 $5
--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for i in data/0*.gv data/1*.gv ; do
+#for i in data/0*.gv data/1*.gv ; do
+for i in data/2*.gv ; do
 	echo $i
 	scripts/g2m.sh $i -max notice
 
@@ -13,9 +14,3 @@ for i in data/0*.gv data/1*.gv ; do
 
 	echo '-------------'
 done
-
-#for i in data/2*.gv; do
-#	echo $i
-#	scripts/g2m.sh $i
-#	echo '-------------'
-#done
