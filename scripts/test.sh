e6d92a920a10bb690ba87c24ddc85a43ec1e83d1
ronsavage <ron@savage.net.au>
2014-09-11 09:37:44 +1000

    Update scripts/test.sh after deleting old copies of Graphviz samples data/90.*

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in data/0*.gv data/1*.gv data/2*.gv data/3*.gv data/4*.gv data/5*.gv data/9*.gv ;
+for i in data/0*.gv data/1*.gv data/2*.gv data/3*.gv data/4*.gv data/5*.gv ;
 do
 
 	if [ "$i" == "data/08.gv" ]
@@ -8,7 +8,7 @@ do
 		echo Expect data/08.gv to be the first to succeed.
 	fi
 
-	scripts/g2m.sh $i -max notice
+	scripts/g2m.sh $i
 
 	if [ "$?" -eq "0" ]
 	then
