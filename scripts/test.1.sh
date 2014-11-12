cd3e82ec84c53154a53d1b6ba01c7cf6a41eaca0
ronsavage <ron@savage.net.au>
2014-11-13 10:27:26 +1100

    Add some graphs from Graphviz to the test suite

--- scripts/test.1.sh
+++ scripts/test.1.sh
@@ -4,7 +4,6 @@
 # 1: The name of input and files.
 #	16 means the input is data/16.gv, and the output is
 #	$DR/Perl-modules/html/graphviz2.marpa/16.svg.
-# 2+: Any parameters accepted by scripts/g2m.sh, i.e. scripts/g2m.pl.
 # $DR is my web server's doc root (in Debian's RAM disk).
 
 echo Contents of data/$1.gv:
@@ -13,7 +12,7 @@ echo ----------------------------
 
 scripts/gv2svg.sh $1
 
-scripts/g2m.sh data/$1.gv $2 $3 $4 $5 $6 $7 $8 $9
+scripts/g2m.sh data/$1.gv -max info -out $1.gv > $1.log
 
 dot -Tsvg data/$1.gv > $DR/Perl-modules/html/graphviz2.marpa/$1.new.svg
 
