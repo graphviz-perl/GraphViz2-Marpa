8b2ccfc09fbd5aa7f7ba70335dd16d7b6cefe74d
ronsavage <ron@savage.net.au>
2014-11-12 13:22:11 +1100

    Clean up scripts/test.1.sh

--- scripts/test.1.sh
+++ scripts/test.1.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Parameters:
+# 1: The name of input and files.
+#	16 means the input is data/16.gv, and the output is
+#	$DR/Perl-modules/html/graphviz2.marpa/16.svg.
+# 2+: Any parameters accepted by scripts/g2m.sh, i.e. scripts/g2m.pl.
 
 echo Contents of data/$1.gv:
 cat data/$1.gv
@@ -9,4 +15,12 @@ then
 	echo Note: $1.gv takes 7 seconds
 fi
 
+scripts/gv2svg.sh $1
+
 scripts/g2m.sh data/$1.gv $2 $3 $4 $5 $6 $7 $8 $9
+
+dot -Tsvg $1.gv > $DR/Perl-modules/html/graphviz2.marpa/$1.new.svg
+
+echo Diff: $DR/Perl-modules/html/graphviz2.marpa/$1.svg $DR/Perl-modules/html/graphviz2.marpa/$1.new.svg
+
+diff $DR/Perl-modules/html/graphviz2.marpa/$1.svg $DR/Perl-modules/html/graphviz2.marpa/$1.new.svg
