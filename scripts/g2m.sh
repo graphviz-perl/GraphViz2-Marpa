48002d6f5b0d4ec8a29dcb619332282671f9cfe3
ronsavage <ron@savage.net.au>
2014-11-20 14:33:30 +1100

    Simplify failing 16/04. Clean up test scripts

--- scripts/g2m.sh
+++ scripts/g2m.sh
@@ -5,4 +5,4 @@
 #	E.g. data/19.gv.
 # 2 .. N: Use for debugging etc. E.g.: -maxlevel debug.
 
-perl -Ilib scripts/g2m.pl -input_file $1 $2 $3 $4 $5
+perl -Ilib scripts/g2m.pl -input_file $1 $2 $3 $4 $5 $6 $7
