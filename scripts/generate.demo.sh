0d7c21f45c58b661c529c3ded27a30f533129d70
ronsavage <ron@savage.net.au>
2014-11-25 09:02:19 +1100

    Change generate.demo.sh to loop over 0 and 1 for author_tests

--- scripts/generate.demo.sh
+++ scripts/generate.demo.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-scripts/generate.svg.sh $1
-
-perl -Ilib scripts/generate.demo.pl -author_tests $1
-
-# $DR is my web server's doc root (in Debian's RAM disk).
-# $PM is a directory path.
-
 PM=Perl-modules/html/graphviz2.marpa
 
-if [ "$1" == "1" ]
-then
-	PM="$PM/author_tests"
-	DIR=xt/author/html
-else
-	DIR=html
-fi
+for author_tests in 0 1
+do
+	scripts/generate.svg.sh $author_tests
 
-cp $DIR/* $DR/$PM             > /dev/null
-cp $DIR/* ~/savage.net.au/$PM > /dev/null
+	perl -Ilib scripts/generate.demo.pl -author_tests $author_tests
 
-echo
+	# $DR is my web server's doc root (in Debian's RAM disk).
+	# $PM is a directory path.
+
+	if [ "$author_tests" == "1" ]
+	then
+		PM="$PM/author_tests"
+		DIR=xt/author/html
+	else
+		DIR=html
+	fi
+
+	cp $DIR/* $DR/$PM             > /dev/null
+	cp $DIR/* ~/savage.net.au/$PM > /dev/null
+
+done
+
 echo Check the version number in the demo index
