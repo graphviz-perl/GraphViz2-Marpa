3dcf8533dc8d638d79e11ddfafc6c4416c43311f
ronsavage <ron@savage.net.au>
2014-11-25 18:06:04 +1100

    Add special cases - files to ignore

--- scripts/find.candidates.pl
+++ scripts/find.candidates.pl
@@ -14,6 +14,27 @@ my($rule) = Path::Iterator::Rule -> new;
 
 my(%seen);
 
+# Skip special cases:
+
+$seen{META}        = 1; # Not a *.gv file.
+$seen{b15}         = 1; # Illegal utf8.
+$seen{b34}         = 1;
+$seen{b56}         = 1;
+$seen{b60}         = 1;
+$seen{Latin1}      = 1;
+$seen{inv_inv}     = 1; # Uses external file (image, *.ps).
+$seen{inv_nul}     = 1;
+$seen{inv_val}     = 1;
+$seen{nul_inv}     = 1;
+$seen{nul_nul}     = 1;
+$seen{nul_val}     = 1;
+$seen{val_inv}     = 1;
+$seen{val_nul}     = 1;
+$seen{val_val}     = 1;
+$seen{pslib}       = 1;
+$seen{user_shapes} = 1;
+$seen{tee}         = 1; # Causes a segfault.
+
 for my $file ($rule -> name(qr/\.gv$/) -> all('./xt/author/data') )
 {
 	$seen{basename($file, '.gv')} = 1;
@@ -28,10 +49,6 @@ my($basename);
 
 while (defined(my $file = $next -> () ) )
 {
-	# Skip special cases:
-	# o Fake *.gv file.
-	# o Already seen.
-
 	$basename = basename($file, '.gv');
 
 	next if ($seen{$basename} || ($file =~ m|tclpkg/gv/META.gv|) );
